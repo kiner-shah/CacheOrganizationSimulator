0605f77bf38585ec9c1b423133188de0b30270da
Kiner Shah <kiner.shah1@gmail.com>
2018-08-15 21:48:25 +0530

    Update and rename example.py to test_direct_mapped_cache.py

--- Direct Mapped Cache/test_direct_mapped_cache.py
+++ Direct Mapped Cache/test_direct_mapped_cache.py
@@ -7,11 +7,11 @@ while True:
 	try:
 		value = int(raw_input("Enter the memory address to be accessed [0 - 16777215]: "))
 		if value < 0 or value > 16777215:
-			print "Value out of range entered"
+			print "Address out of range entered"
 			continue
 		memory.search(value)
 		x = raw_input("Continue (yes / no): ")
 		if x == "no" or x == "n" or x == "NO" or x == "No":
 			break
 	except ValueError:
-		print "Illegal value entered"
+		print "Illegal address entered"
